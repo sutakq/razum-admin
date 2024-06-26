664d97817746ee8ef98f88af9369183a3494c4e4
sutakq <96618229+sutakq@users.noreply.github.com>
2024-06-26 05:15:19 +0300

    Update connect.php

--- connect/connect.php
+++ connect/connect.php
@@ -2,10 +2,10 @@
 // $database = new PDO("mysql:host=localhost;charset=utf8;dbname=razum10_platform", 'razum10', 'Pzjhw6bJ');
 
 try{
-    $host = 'localhost';
-    $dbname = 'razumNeiro';
-    $user = 'root';
-    $pass = '';
+    $host = '109.68.213.4';
+    $dbname = 'razumplatform';
+    $user = 'admin platform';
+    $pass = 'q*TG61mg,sB+EB';
     $database = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8;", $user, $pass);
     return $database;
 }
